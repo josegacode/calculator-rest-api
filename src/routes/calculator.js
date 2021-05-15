23ba147c2189621b15f3f02a10de504c8a1de7fc
Jose Garcia <josegarcia.dev@gmail.com>
2021-05-15 12:24:27 -0500

    Extracts expressions like (A + B) into an array like ['A', '+', 'B']

--- src/routes/calculator.js
+++ src/routes/calculator.js
@@ -1,14 +1,8 @@
-const { Router, request, response } = require('express');
+const { Router } = require('express');
 const router = new Router();
 
 // / will be replaced with /api/v1/operation in general routes
 router.post('/', (request, response) => {
-  //console.log(`CONTENT: ${JSON.stringify(request.body)}`);
-  //response.json({"test": "value"});                                                                         
-
-  // Extracts the body from request.
-  // It contains the data sent by
-  // client.
   const { body } = request
   response.send(body)
 })
